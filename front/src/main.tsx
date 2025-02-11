5ae1375561c55374aca9469f312833d203914529
dnlMatos <georginedanilo@gmail.com>
2025-02-10 21:22:42 -0300

    � chore: update dependencies and improve typescript configuration

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
-import "../src/index.css";
+import "./index.css";
 import App from "./App.tsx";
 import AuthProvider from "./providers/AuthProvider.tsx";
 
