8c93dca68d1ca94ce976b471f3e2417b298b8b61
Danilo Matos <60492862+dnlMatos@users.noreply.github.com>
2025-02-11 11:06:26 -0300

    Update user.route.js

--- back/src/routes/user.route.js
+++ back/src/routes/user.route.js
@@ -1,22 +1,11 @@
-// import { Router } from "express";
-// import { protectRoute } from "../middleware/auth.middleware.js";
-// import { getAllUsers, getMessages } from "../controller/user.controller.js";
-
-// // Define rotas para o endpoint de usuários
-// const router = Router();
-
-// router.get("/", protectRoute, getAllUsers);
-// router.get("/messages/:userId", protectRoute, getMessages);
-
-// export default router;
-
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllUsers } from "../controller/user.controller.js";
+import { getAllUsers, getMessages } from "../controller/user.controller.js";
 
+// Define rotas para o endpoint de usuários
 const router = Router();
 
 router.get("/", protectRoute, getAllUsers);
+router.get("/messages/:userId", protectRoute, getMessages);
 
-//get all messages
 export default router;
